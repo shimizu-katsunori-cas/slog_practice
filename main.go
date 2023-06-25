2b8dc592a75cce329d45428e91e81d7115228e32
shimizu-katsunori-cas <katsunori.shimizu@casting-one.jp>
2023-06-25 21:16:01 +0900

    slogtestの実装

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
+	"log"
 	"log/slog"
 	"os"
+	"testing/slogtest"
 )
 
 func main() {
@@ -97,4 +101,30 @@ func main() {
 	/** 9. 出力結果
 	{"time":"2023-06-25T20:42:08.558559+09:00","level":"ERROR","msg":"9.ログレベルがErrorです"}
 	*/
+	// 10 slogtestの利用
+	slogTest()
+}
+
+func slogTest() {
+	var buf bytes.Buffer
+	h := slog.NewJSONHandler(&buf, nil)
+
+	results := func() []map[string]any {
+		var ms []map[string]any
+		for _, line := range bytes.Split(buf.Bytes(), []byte{'\n'}) {
+			if len(line) == 0 {
+				continue
+			}
+			var m map[string]any
+			if err := json.Unmarshal(line, &m); err != nil {
+				panic(err) // In a real test, use t.Fatal.
+			}
+			ms = append(ms, m)
+		}
+		return ms
+	}
+	err := slogtest.TestHandler(h, results)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
